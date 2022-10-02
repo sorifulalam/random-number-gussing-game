18f5df0c7534bef5733ab33587ec96cb06fb3ba0
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-10-02 19:47:06 +0600

    step-1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
- //const  generate = Math.floor(Math.random() * 10) + 1;
+//Cought to showing paharagrap...
 const wonGuessing = document.getElementById("wonGuessing");
+
+//Click And condition full fill result show code ... 
 
 function getNumber(){
     const getNumber =document.getElementById("getNumber").value;
@@ -12,13 +14,26 @@ function getNumber(){
     if(displayNumber == enterGenerater){
         
         alert("Congratulations You Have Won This Game", wonGuessing.innerHTML=`You Have Won!!! Random Number Was ${enterGenerater}`);
+        winCount++
     }
    else{
        wonGuessing.innerHTML=`You Have lose!!! Random Number Was ${enterGenerater}`;
+       lossCount++
     }
 } 
+
+//Randome Number Generate ...
 
 function generateNUmber(){
     return Math.floor(Math.random() * 10) + 1;
 }
+//Won Loos count 
+const wonCount = document.getElementById("wonCount");
+const loseCount = document.getElementById("loseCount");
+
+let  winCount= 0;
+let lossCount = 0;
+
+wonCount.innerHTML(` Number of Won : ${winCount}`)
+loseCount.innerHTML(` Number of Lost : ${lossCount}`)
 
