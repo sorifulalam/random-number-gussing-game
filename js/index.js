1db42fd447a3a4f31ef84a710bbd1fc61ce351bf
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-10-02 15:59:01 +0600

    mid l

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-const  generate = Math.floor(Math.random() * 10);
+ //const  generate = Math.floor(Math.random() * 10) + 1;
 const wonGuessing = document.getElementById("wonGuessing");
 
 function getNumber(){
-    const getNumber =document.getElementById("getNumber").value= " ";
+    const getNumber =document.getElementById("getNumber").value;
+    const displayNumber = parseFloat(getNumber)
+   console.log(displayNumber);
+    document.getElementById("getNumber").value="";
+    const enterGenerater =generateNUmber();
+    console.log(enterGenerater);
 
-    if(generate == getNumber){
-        wonGuessing.innerHTML=`You Have Won!!! Random Number Was ${generate}`;
+    if(displayNumber == enterGenerater){
+        
+        alert("Congratulations You Have Won This Game", wonGuessing.innerHTML=`You Have Won!!! Random Number Was ${enterGenerater}`);
+    }
+   else{
+       wonGuessing.innerHTML=`You Have lose!!! Random Number Was ${enterGenerater}`;
+    }
+} 
 
-    }
-    else{
-        wonGuessing.innerHTML=`You Have lose!!! Random Number Was ${generate}`;
-    }
+function generateNUmber(){
+    return Math.floor(Math.random() * 10) + 1;
 }
+
