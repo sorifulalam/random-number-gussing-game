c5a23d7819551cc5f64ad45397422b96518c0ef2
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-10-03 17:05:35 +0600

    u

--- js/index.js
+++ js/index.js
@@ -1,24 +1,53 @@
+
+let  winCount= 0;
+let lossCount = 0;
+let ranNumber =0;
+
+function generateNew(){
+    ranNumber = generateNUmber();
+}
+
+function Clear(){
+    
+    document.getElementById("wonCount").innerText="0";
+    document.getElementById("loseCount").innerText="0";
+    winCount= 0;
+    lossCount = 0;
+
+}
+
+// const wonCount = document.getElementById("wonCount").innerText;
+// const addwonCount = parseFloat(wonCount)
+
+// const loseCount = document.getElementById("loseCount").innerText;
+// const addlosCount= parseFloat(loseCount);
+// const totalCount = lossCount+addlosCount;
+
+
+// console.log(totalCount)
 //Cought to showing paharagrap...
 const wonGuessing = document.getElementById("wonGuessing");
 
 //Click And condition full fill result show code ... 
 
 function getNumber(){
-    const getNumber =document.getElementById("getNumber").value;
-    const displayNumber = parseFloat(getNumber)
-   console.log(displayNumber);
+    const getNumber =parseFloat(document.getElementById("getNumber").value);
+   console.log(getNumber);
     document.getElementById("getNumber").value="";
-    const enterGenerater =generateNUmber();
-    console.log(enterGenerater);
+    console.log(ranNumber);
 
-    if(displayNumber == enterGenerater){
+    if(getNumber == ranNumber){
         
-        alert("Congratulations You Have Won This Game", wonGuessing.innerHTML=`You Have Won!!! Random Number Was ${enterGenerater}`);
-        winCount++
+        alert("Congratulations You Have Won This Game", wonGuessing.innerHTML=`You Have Won!!! Random Number Was ${ranNumber}`);
+        winCount++;
+        document.getElementById("wonCount").innerText=winCount;
+        
     }
    else{
-       wonGuessing.innerHTML=`You Have lose!!! Random Number Was ${enterGenerater}`;
-       lossCount++
+       wonGuessing.innerHTML=`You Have lose!!! Random Number Was ${ranNumber} total win= ${winCount} total loss= ${lossCount}`;
+       lossCount++;
+       document.getElementById("loseCount").innerText=lossCount;
+       
        
     }
    
@@ -30,24 +59,91 @@ function generateNUmber(){
     return Math.floor(Math.random() * 10) + 1;
 }
 //Won Loos count 
-const wonCount = document.getElementById("wonCount").value;
-const addwonCount = parseFloat(wonCount)
-const loseCount = document.getElementById("loseCount").value;
-const addlosCount= parseFloat(loseCount)
 
-let  winCount= 0;
-let lossCount = 0;
+
+
+//document.getElementById("loseCount").innerText = totalCount
+
+
+
 
 //  document. wonCount.innerHTML(` Number of Won : ${winCount}`)
 // document. loseCount.innerHTML(` Number of Lost : ${lossCount}`)
 
 
-function go(id  ){
-    const curent = document.getElementById(id).innerText
-    const curentNumber = parseFloat(curent)
-   const total = loseCount+lossCount
+// function go(id  ){
+//     const curent = document.getElementById(id).innerText
+//     const curentNumber = parseFloat(curent)
+//    const total = loseCount+lossCount
    
-}
+// }
+
+
+// let  winCount= 0;
+// let lossCount = 0;
+
+// const wonCount = document.getElementById("wonCount").innerText;
+// const addwonCount = parseFloat(wonCount)
+
+// const loseCount = document.getElementById("loseCount").innerText;
+// const addlosCount= parseFloat(loseCount);
+// const totalCount = lossCount+addlosCount;
+
+// console.log(totalCount)
+// //Cought to showing paharagrap...
+// const wonGuessing = document.getElementById("wonGuessing");
+
+// //Click And condition full fill result show code ... 
+
+// function getNumber(){
+//     const getNumber =document.getElementById("getNumber").value;
+//     const displayNumber = parseFloat(getNumber)
+//    console.log(displayNumber);
+//     document.getElementById("getNumber").value="";
+//     const enterGenerater =generateNUmber();
+//     console.log(enterGenerater);
+
+//     if(displayNumber == enterGenerater){
+        
+//         alert("Congratulations You Have Won This Game", wonGuessing.innerHTML=`You Have Won!!! Random Number Was ${enterGenerater}`);
+//         winCount++;
+        
+//     }
+//    else{
+//        wonGuessing.innerHTML=`You Have lose!!! Random Number Was ${enterGenerater} total win= ${winCount} total loss= ${lossCount}`;
+//        lossCount++;
+       
+       
+//     }
+   
+// } 
+
+// //Randome Number Generate ...
+
+// function generateNUmber(){
+//     return Math.floor(Math.random() * 10) + 1;
+// }
+// //Won Loos count 
+
+
+
+// //document.getElementById("loseCount").innerText = totalCount
+
+
+
+
+// //  document. wonCount.innerHTML(` Number of Won : ${winCount}`)
+// // document. loseCount.innerHTML(` Number of Lost : ${lossCount}`)
+
+
+// // function go(id  ){
+// //     const curent = document.getElementById(id).innerText
+// //     const curentNumber = parseFloat(curent)
+// //    const total = loseCount+lossCount
+   
+// // }
+
+
 
 
 
